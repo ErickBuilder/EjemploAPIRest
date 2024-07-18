a4f6d9c4b254802ac0257a72a6475c9645b0f683
ErickBuilder <erick.aguilar.soto@gmail.com>
2024-07-18 00:01:01 -0600

    Add project files.

---
+++ OperacionesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace EjemploAPIRest
+{
+    [Route("Operaciones")]
+    [ApiController]
+    public class OperacionesController : ControllerBase
+    {
+        [Route("CalcularMayorDe2")]
+        [HttpGet]
+        public IActionResult CalcularNumeroMayor(int numero1, int numero2)
+        {
+            int valorMayor = 0;
+
+            if (numero1 == numero2) {   valorMayor = numero1; }
+
+            if (numero1 > numero2)  {   valorMayor = numero1; }
+
+            if (numero2 > numero1)  {   valorMayor = numero2; }
+
+            return Ok(new { resultado = $"El numero enviado es el {valorMayor}" });
+        }
+
+    }
+}
